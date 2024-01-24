a3a98e4be6e265296eb2d2c3afea6f235ea6d0fc
Khushi Sidana <sidana.khushi@gmail.com>
2024-01-24 16:31:41 -0600

    original

--- mail-automation.py
+++ mail-automation.py
@@ -11,27 +11,4 @@ EMAIL_SERVER = "smtp.gmail.com"
 
 #Load the environment variables 
 
-current_dir = Path(__file__).resolve().parent if "_file_" in locals() else Path.cwd()
-envars = current_dir / ".env"
-load_dotenv(envars)
-
-sender_email = os.getenv("EMAIL")
-password_email = os.getenv("PASSWORD")
-
-def send_email(subject, receiver_email, due_date, invoice_no, amount):
-    msg= EmailMessage()
-    msg["Subject"] = subject
-    msg["From"] = formataddr("Trying out the messenger", f"{send_email}")
-    msg["To"] = receiver_email
-    msg["BCC"] = sender_email
-
-    msg.set_content(
-        f"""\
-        Hi {name},
-I hope you are well.
-I just wanted to drop you a quick note to remind you that {amount} USD in respect of our invoice {invoice_no} is due for payment on {due_date}
-I would be really grateful if you could confirm that everything is on track for payment.
-Best regards
-Khushi Sidana
-"""
-)
+c
