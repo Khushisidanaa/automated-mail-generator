893ca2f827a03d6c1fa33f57fd212fbc37aabb49
Khushi Sidana <khushisidana@Khushis-MacBook-Air-3.local>
2024-01-24 16:15:21 -0600

    Added static text

--- mail-automation.py
+++ mail-automation.py
@@ -1,2 +1,37 @@
-print('hi')
-print('bye')
+import os
+import smtplib
+from email.message import EmailMessage
+from email.utils import formataddr
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+PORT = 587
+EMAIL_SERVER = "smtp.gmail.com"
+
+#Load the environment variables 
+
+current_dir = Path(__file__).resolve().parent if "_file_" in locals() else Path.cwd()
+envars = current_dir / ".env"
+load_dotenv(envars)
+
+sender_email = os.getenv("EMAIL")
+password_email = os.getenv("PASSWORD")
+
+def send_email(subject, receiver_email, due_date, invoice_no, amount):
+    msg= EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = formataddr("Trying out the messenger", f"{send_email}")
+    msg["To"] = receiver_email
+    msg["BCC"] = sender_email
+
+    msg.set_content(
+        f"""\
+        Hi {name},
+I hope you are well.
+I just wanted to drop you a quick note to remind you that {amount} USD in respect of our invoice {invoice_no} is due for payment on {due_date}
+I would be really grateful if you could confirm that everything is on track for payment.
+Best regards
+Khushi Sidana
+"""
+)
